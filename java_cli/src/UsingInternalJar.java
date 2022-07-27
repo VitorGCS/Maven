2d55adc49c9b27addac83ed48c69cdb78e907159
Vitor Silva <vitorgilcs@gmail.com>
2022-07-27 12:26:10 +0100

    Creating my jar file and using in the main application

---
+++ java_cli/src/UsingInternalJar.java
@@ -0,0 +1,16 @@
+//import mathLibrary.MathOperations;
+
+public class UsingInternalJar {
+    public static void main(String [] args){
+        int first = 10;
+        int second = 2;
+/*
+        System.out.println(first+" + "+second+" = "+ MathOperations.sum(first, second) );
+        System.out.println(first+" - "+second+" = "+ MathOperations.sub(first, second) );
+        System.out.println(first+" * "+second+" = "+ MathOperations.mul(first, second) );
+        System.out.println(first+" / "+second+" = "+ MathOperations.div(first, second) );
+*/
+    }
+
+}
+
